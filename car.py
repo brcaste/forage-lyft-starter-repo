9aa28668f3e3eb6589125e0fd002744b09c8da49
Brandon Castellanos <bcas252@wgu.edu>
2023-11-29 22:53:36 -0600

    Refactoring codebase

--- car.py
+++ car.py
@@ -1,10 +1,14 @@
-from abc import ABC, abstractmethod
+from battery.battery import Battery
+from engine.engine import Engine
+from serviceable import Serviceable
 
 
-class Car(ABC):
+class Car(Serviceable, Battery, Engine):
+    engine_cls = Engine
+    battery_cls = Battery
+
     def __init__(self, last_service_date):
         self.last_service_date = last_service_date
 
-    @abstractmethod
     def needs_service(self):
         pass
